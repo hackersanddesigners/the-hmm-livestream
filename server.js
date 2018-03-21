e0983e2e5aa329c561aecf8909511d48602f4580
Glitch (hello-sqlite) <none>
2018-03-21 18:20:37 +0000

    🐿🌞 Checkpoint ./package.json:96831/2821 ./.env:96831/85 ./server.js:96831/311

--- server.js
+++ server.js
@@ -3,13 +3,22 @@
 
 // init project
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+// init sqlite db
+var fs = require('fs');
+var dbFile = './.data/sqlite.db';
+var exists = fs.existsSync(dbFile);
+var sqlite3 = require('sqlite3').verbose();
+var db
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
