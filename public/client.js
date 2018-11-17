38756a4d77993a7902bd4a0c7c322b3b2e19a72a
Glitch (hello-sqlite) <none>
2018-11-17 08:32:00 +0000

    💦🏊🏼 Checkpoint ./public/client.js:1834082/769

--- public/client.js
+++ public/client.js
@@ -3,6 +3,16 @@ const m = window.m;
 
 const App = {
   count: 0,
+  dreams: [],
+  getDreams: () => {
+    m.request({
+      method: "GET",
+      url: "/getDreams",
+    })
+    .then(function(result) {
+      App.dreams = result;
+    })
+  },
   view: function() {
     return [
       m('header', [
@@ -10,6 +20,7 @@ const App = {
       ]),
       m("main", [
         m("h1", {class: "title"}, `Num: ${App.count}`),
+        m("button", { onclick: App.getDreams }, "Get Dreams"),
         m("button", { onclick: () => (App.count = App.count + 1) }, "A button"),
       ]),
     ];
