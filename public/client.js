3460c721f671d60252572166707daef12f6e08ea
Glitch (hello-sqlite) <none>
2018-11-17 08:00:37 +0000

    🌚🍤 Checkpoint ./public/client.js:1834082/185

--- public/client.js
+++ public/client.js
@@ -2,14 +2,15 @@ const root = document.body
 const m = window.m;
 
 const App = {
+  count: 0,
   view: function() {
     return [
       m('header', [
         m('h1', 'Tubelet'),
       ]),
       m("main", [
-        m("h1", {class: "title"}, "My first app"),
-        m("button", "A button"),
+        m("h1", {class: "title"}, `Num: ${App.count}`),
+        m("button", { onclick: () => (App.count = App.count + 1) }, "A button"),
       ]),
     ];
   }
