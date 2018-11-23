0ac52b628b0e63091a51255116454ca819793759
Glitch (hello-sqlite) <none>
2018-11-23 21:39:48 +0000

    🛢🙌 Checkpoint ./package.json:1834082/1457 ./server.js:1834082/630

--- server.js
+++ server.js
@@ -14,35 +14,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // init sqlite db
-var fs = require('fs');
+var SQL = require('sql-template-strings');
 var dbFile = './.data/sqlite.db';
-var exists = fs.existsSync(dbFile);
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(dbFile);
+var sqlite = require('sqlite');
 
 const Mux = require('@mux/mux-node');
 const { Video } = new Mux(process.env.MUX_TOKEN_ID, process.env.MUX_TOKEN_SECRET);
 
+const initializeDatabase = async () => {
+  try {
+    const db = await sqlite.open(dbFile);
+    const data = await db.all(SQL``);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
-db.serialize(function(){
-  if (!exists) {
-    db.run('CREATE TABLE Dreams (dream TEXT)');
-    console.log('New table Dreams created!');
+// db.serialize(function(){
+//   if (!exists) {
+//     db.run('CREATE TABLE Dreams (dream TEXT)');
+//     console.log('New table Dreams created!');
     
-    // insert default dreams
-    db.serialize(function() {
-      db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
-    });
-  }
-  else {
-    console.log('Database "Dreams" ready to go!');
-    db.each('SELECT * from Dreams', function(err, row) {
-      if ( row ) {
-        console.log('record:', row);
-      }
-    });
-  }
-});
+//     // insert default dreams
+//     db.serialize(function() {
+//       db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
+//     });
+//   }
+//   else {
+//     console.log('Database "Dreams" ready to go!');
+//     db.each('SELECT * from Dreams', function(err, row) {
+//       if ( row ) {
+//         console.log('record:', row);
+//       }
+//     });
+//   }
+// });
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
