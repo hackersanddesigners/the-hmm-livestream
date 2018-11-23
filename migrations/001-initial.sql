f13d53321fad2d296cac23b9e50551212d59549d
Glitch (hello-sqlite) <none>
2018-11-23 21:55:48 +0000

    🐑🙎 Checkpoint ./server.js:1834082/4258 ./migrations/001-initial.sql:1834082/1671

---
+++ migrations/001-initial.sql
@@ -0,0 +1,5 @@
+-- Up
+CREATE TABLE Stream (id INTEGER PRIMARY KEY, streamKey TEXT, streamId TEXT);
+
+-- Down
+DROP TABLE Stream
--- server.js
+++ server.js
@@ -1,55 +1,20 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+const Mux = require('@mux/mux-node');
+const { Video } = new Mux(process.env.MUX_TOKEN_ID, process.env.MUX_TOKEN_SECRET);
 
 // init sqlite db
 var SQL = require('sql-template-strings');
 var dbFile = './.data/sqlite.db';
 var sqlite = require('sqlite');
 
-const Mux = require('@mux/mux-node');
-const { Video } = new Mux(process.env.MUX_TOKEN_ID, process.env.MUX_TOKEN_SECRET);
-
-const initializeDatabase = async () => {
-  try {
-    const db = await sqlite.open(dbFile);
-    const data = await db.all(SQL``);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
-// db.serialize(function(){
-//   if (!exists) {
-//     db.run('CREATE TABLE Dreams (dream TEXT)');
-//     console.log('New table Dreams created!');
-    
-//     // insert default dreams
-//     db.serialize(function() {
-//       db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
-//     });
-//   }
-//   else {
-//     console.log('Database "Dreams" ready to go!');
-//     db.each('SELECT * from Dreams', function(err, row) {
-//       if ( row ) {
-//         console.log('record:', row);
-//       }
-//     });
-//   }
-// });
+const dbPromise = Promise.resolve()
+  .then(() => sqlite.open(dbFile))
+  .then(db => db.migrate({ force: 'last' }));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
